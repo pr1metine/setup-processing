2e4aae2fd501088a9ca595f91f20ce51dae3e0dc
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 17:40:02 +0200

    Alle awaits gestrichen

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const tc = require('@actions/tool-cache')
 
 try {
     const procPath = tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
-    const procExtractedFolder = await tc.extractTar(procPath, '/usr/bin/processing') 
+    const procExtractedFolder =  tc.extractTar(procPath, '/usr/bin/processing') 
     
-    const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
+    const cachedPath =  tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
     core.addPath(cachedPath) 
 } catch (error) {
     core.setFailed(error.message) 
