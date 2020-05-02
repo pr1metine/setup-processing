e0ac2fd892d359ae6148f42177a2762021d328fb
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 17:35:57 +0200

    await einmal rausgestrichen

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 const tc = require('@actions/tool-cache') 
 
 try {
-    const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
+    const procPath = tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
     const procExtractedFolder = await tc.extractTar(procPath, '/usr/bin/processing') 
     
     const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
