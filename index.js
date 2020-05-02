0bca0fc8d16910274b8f07661245e0fcd5a3112a
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 17:49:22 +0200

    awaits in processing gestrichen

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// const core = require('@actions/core') 
-// const tc = require('@actions/tool-cache') 
+const core = require('@actions/core') 
+const tc = require('@actions/tool-cache') 
 
-// try {
-//     const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
-//     const procExtractedFolder = await tc.extractTar(procPath, '/usr/bin/processing') 
+try {
+    const procPath = tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
+    const procExtractedFolder = tc.extractTar(procPath, '/usr/bin/processing') 
     
-//     const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
-//     core.addPath(cachedPath) 
-// } catch (error) {
-//     core.setFailed(error.message) 
-// }
+    const cachedPath = tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
+    core.addPath(cachedPath) 
+} catch (error) {
+    core.setFailed(error.message) 
+}
 
-const tc = require('@actions/tool-cache');
-const core = require('@actions/core');
+// const tc = require('@actions/tool-cache');
+// const core = require('@actions/core');
 
-const node12Path =  tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz');
-const node12ExtractedFolder =  tc.extractTar(node12Path, 'path/to/extract/to');
+// const node12Path =  tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz');
+// const node12ExtractedFolder =  tc.extractTar(node12Path, 'path/to/extract/to');
 
-const cachedPath =  tc.cacheDir(node12ExtractedFolder, 'node', '12.7.0');
-core.addPath(cachedPath);
+// const cachedPath =  tc.cacheDir(node12ExtractedFolder, 'node', '12.7.0');
+// core.addPath(cachedPath);
