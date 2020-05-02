3ee1ae0b900a9e0cfe1671d0439255af425de0d9
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 17:30:17 +0200

    console.log am Ende und ohne Variablen

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const tc = require('@actions/tool-cache');
 
 try {
     const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz');
-    console.log(`procPath: ${procPath}`);
 
     const procExtractedFolder = await tc.extractTar(procPath, '/usr/bin/processing');
-    console.log(`procExtractedFolder: ${procExtractedFolder}`);
     
     const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4');
-    console.log(`cachedPath: ${cachedPath}`);
+
     core.addPath(cachedPath);
+
+    console.log('procPath');
 } catch (error) {
     core.setFailed(error.message);
 }
