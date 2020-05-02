4247c9d61857bb2a2ebf342a8785fd206e195b09
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 16:31:37 +0200

    Action implementiert: Test 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const tc = require('@actions/tool-cache')
+
+try {
+    const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz')
+    const procExtractedFolder = await tc.extractTar(procPath, '/usr/bin/processing')
+    
+    const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4')
+    core.addPath(cachedPath)
+} catch (error) {
+    core.setFailed(error.message)
+}
