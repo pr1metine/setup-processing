108cd5e8780eb1ae9a78228ffdabd5a04fae47aa
Thanh <quang.thanh.ta@pr1meti.de>
2020-05-02 19:30:01 +0200

    Caching und /home/pr1metine/.vscode-server/bin/ff915844119ce9485abfe8aa9076ec76b5300ddd/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files/Amazon/AWSCLIV2/:/mnt/c/Users/derth/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/derth/AppData/Local/Programs/Microsoft VS Code/bin

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-const core = require('@actions/core') 
-const tc = require('@actions/tool-cache') 
+const core = require('@actions/core')
+const tc = require('@actions/tool-cache')
 
-async function run(){
+async function run() {
+    if (tc.find('processing', '3.5.4', 'x64')) {
+        console.log('Processing is already cached.')
+        return;
+    }
+
     try {
         console.log('Downloading Processing 3.5.4...')
-        const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz') 
+        const procPath = await tc.downloadTool('https://download.processing.org/processing-3.5.4-linux64.tgz')
         console.log(`Downloaded: ${procPath}`)
-    
+
         console.log('Extracting...')
-        const procExtractedFolder = await tc.extractTar(procPath, 'processing') 
+        const procExtractedFolder = await tc.extractTar(procPath, 'processing')
         console.log(`Extracted: ${procExtractedFolder}`)
-    
+
         console.log('Caching...')
-        const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4') 
-        core.addPath(cachedPath) 
+        const cachedPath = await tc.cacheDir(procExtractedFolder, 'processing', '3.5.4')
+        core.addPath(cachedPath)
         console.log(`Cached and added to path ${cachedPath}`)
     } catch (error) {
-        core.setFailed(error.message) 
+        core.setFailed(error.message)
     }
 }
 
 run();
-
-// const tc = require('@actions/tool-cache');
-// const core = require('@actions/core');
-
-// const tc = require('@actions/tool-cache');
-// const core = require('@actions/core');
-
-// const node12Path = await tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz');
-// const node12ExtractedFolder = await tc.extractTar(node12Path, 'path/to/extract/to');
-
-// const cachedPath = await tc.cacheDir(node12ExtractedFolder, 'node', '12.7.0');
-// core.addPath(cachedPath);
