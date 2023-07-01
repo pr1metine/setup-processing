43546321708048ff266b3e3aff0b9e265c14f59d
Thanh <quang.thanh.ta@pr1meti.de>
2023-07-02 00:34:41 +0200

    feat!: Setup Processing 4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import { Octokit, RestEndpointMethodTypes } from "@octokit/rest";
+import * as fs from "fs";
+import { coerce } from "semver";
 
 let OCTOKIT = new Octokit();
 
@@ -49,12 +51,6 @@ async function getReleaseByTag(
     );
   }
 
-  core.debug(
-    `getReleaseByTag: repos=${JSON.stringify(repos)}, tag=${JSON.stringify(
-      tag
-    )}`
-  );
-
   const isFulfilled = <T>(
     input: PromiseSettledResult<T>
   ): input is PromiseFulfilledResult<T> => input.status === "fulfilled";
@@ -74,12 +70,6 @@ async function getReleaseByTag(
 
   let successfullyRetrievedReleases = releases.filter(
     (release) => release.status >= 200 && release.status < 300
-  );
-
-  core.debug(
-    `Successfully retrieved releases: ${JSON.stringify(
-      successfullyRetrievedReleases
-    )}`
   );
 
   if (successfullyRetrievedReleases.length < 1) {
@@ -118,63 +108,130 @@ async function getReleaseByTag(
   }
 }
 
-async function downloadAsset(
+async function installAsset(
+  tag: string,
   assets: ReleaseAsset[],
   archiveName?: string
-): Promise<string> {
-  if (archiveName !== undefined) {
-    let asset = assets.find((asset) => asset.name === archiveName);
-    if (asset === undefined) {
-      throw new Error(`The given archive name does not exist: ${archiveName}`);
+) {
+  let asset = getAssetByName(assets, archiveName);
+  if (asset === undefined) {
+    throw new Error(
+      `The given archive name does not exist: ${
+        archiveName ?? "(Infered, please open an issue for this)"
+      }`
+    );
+  }
+  core.info(`Downloading release asset "${asset.name}"`);
+
+  let path = await extractTool(
+    await tc.downloadTool(asset.url),
+    asset.content_type
+  );
+
+  await cacheProcessingBinary(path, tag);
+
+  async function cacheProcessingBinary(extractedDir: string, tag: string) {
+    let childEntries = await fs.promises.opendir(extractedDir);
+    let next = childEntries.readSync();
+
+    if (next === null || next.isFile() || next.name === undefined) {
+      throw new Error(
+        `Unexpected folder structure of extraced Processing archive. Please open an issue`
+      );
+    }
+    let subFolder = core.toPlatformPath(`${extractedDir}/${next.name}`);
+    let cachedPath = await tc.cacheDir(subFolder, "processing", tag);
+    core.addPath(cachedPath);
+  }
+  function getAssetByName(
+    assets: ReleaseAsset[],
+    assetName?: string
+  ): ReleaseAsset | undefined {
+    if (assetName === undefined) {
+      core.info(`Infering correct asset file name...`);
+      let matchers = getMatchers(process.platform, process.arch);
+      return assets.find((asset) =>
+        matchers.every((matcher) => asset.name.match(matcher) !== null)
+      );
     }
 
-    return extractTool(await tc.downloadTool(asset.url), asset.content_type);
+    return assets.find((asset) => asset.name === assetName);
   }
-
-  core.info(`Determining correct asset file name...`);
-  getMatchers(process.platform, process.arch);
-
-  return "what?";
-
   function getMatchers(
     os: NodeJS.Platform,
     arch: NodeJS.Architecture
   ): RegExp[] {
+    let out = new Array<RegExp>();
     switch (os) {
       case "win32":
-      case "cygwin":
-
+        out.push(/.*windows.*/);
+        break;
       case "linux":
-      case "android":
-
+        out.push(/.*linux.*/);
+        break;
       case "darwin":
-
-      case "aix":
-      case "freebsd":
-      case "haiku":
-      case "openbsd":
-      case "sunos":
-      case "netbsd":
+        out.push(/.*macos.*/);
+        break;
+      default:
+        throw new Error(`Unsupported platform ${os}. Please open an issue`);
     }
-    return [];
+
+    switch (arch) {
+      case "arm":
+        out.push(/.*(arm32|armv6hf).*/);
+        break;
+      case "arm64":
+        out.push(/.*(arm64|aarch64).*/);
+        break;
+      case "x64":
+        out.push(/.*(x64|linux64|windows64|macosx).*/);
+        break;
+      // Apparently no x86 32 bit
+      default:
+        throw new Error(
+          `Unsupported architecture ${arch}. Please open an issue`
+        );
+    }
+
+    return out;
   }
   function extractTool(path: string, type: ArchiveType): Promise<string> {
     switch (type) {
       case "application/zip":
-        return tc.extractZip(path);
+        return tc.extractZip(path, "extracted-processing");
       case "application/gzip":
-        return tc.extractTar(path);
+        return tc.extractTar(path, "extracted-processing");
     }
   }
 }
 
 async function run() {
-  const release = await getReleaseByTag(REPOSITORIES, "processing-1292-4.2");
-  core.debug(`Release: ${JSON.stringify(release)}`);
-  const installPath = await downloadAsset(
-    release.assets,
-    "processing-4.2-linux-x64.tgz"
+  const tag = core.getInput("tag") || undefined;
+  const assetName = core.getInput("asset-name") || undefined;
+  core.debug(
+    `Attempting to find release with the following tag: ${
+      tag ?? "(Infered, none given)"
+    }`
   );
+  const release = await getReleaseByTag(REPOSITORIES, tag);
+  core.debug(`Found release: ${JSON.stringify(release)}`);
+
+  core.debug(`Trying to locate existing Processing installation`);
+  let semverTag = coerce(release.tag)!.format();
+  let cachedDirectory = tc.find("processing", semverTag);
+  if (cachedDirectory) {
+    core.info(`Processing (${release.tag}) is already cached. Aborting...`);
+    core.addPath(cachedDirectory);
+    return;
+  }
+
+  core.debug(
+    `Attempting to download release asset with the following assetName: ${
+      assetName ?? "(Infered, none given)"
+    }`
+  );
+  const installPath = await installAsset(semverTag, release.assets, assetName);
+  core.debug(`Processing successfully installed!`);
 }
 
 try {
